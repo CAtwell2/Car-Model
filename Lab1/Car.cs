9d30422902f86e65445c9020011dd1b44ce5b804
Caden <Caden@DESKTOP-FM9BGJ0>
2023-02-19 23:06:03 -0500

    Add project files.

---
+++ Lab1/Car.cs
@@ -0,0 +1,195 @@
+﻿
+using System.Runtime.CompilerServices;
+
+public class Car
+{
+    //fields
+    private string make;
+    private string model;
+    private int year;
+    private double maximumGasTankLevel;
+    private double currentGasTankLevel;
+    private double milesPerGallon;
+
+    // public properties (getters and setters)
+    public string Make
+    {
+        get { return make; }
+        set { make = value; }
+    }
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+    //setting parameters around the year to determine if it was a good input or not
+    public int Year
+    {
+        get { return year; }
+        set
+        {
+            if (value >= 1900 && value <= DateTime.Now.Year + 1)
+            {
+                year = value;
+            }
+            else
+            {
+                year = DateTime.Now.Year;
+                Console.WriteLine("Invalid year. Year set to current year.");
+            }
+        }
+    }
+    //doing the same thing as with the year but this time with the gas levels
+    public double MaximumGasTankLevel
+    {
+        get { return maximumGasTankLevel; }
+        set
+        {
+            if (value >= 0)
+            {
+                maximumGasTankLevel = value;
+            }
+            else
+            {
+                maximumGasTankLevel = 0;
+                Console.WriteLine("Invalid maximum gas tank level. Level set to 0.");
+            }
+        }
+    }
+
+    public double CurrentGasTankLevel
+    {
+        get { return currentGasTankLevel; }
+        set
+        {
+            if (value >= 0)
+            {
+                currentGasTankLevel = value;
+            }
+            else
+            {
+                currentGasTankLevel = 0;
+                Console.WriteLine("Invalid current gas tank level. Level set to 0.");
+            }
+        }
+    }
+    //letting user input mpg, if it = 0 it will tell them its invalid
+    public double MilesPerGallon
+    {
+        get { return milesPerGallon; }
+        set
+        {
+            if (value >= 0)
+            {
+                milesPerGallon = value;
+            }
+            else
+            {
+                milesPerGallon = 0;
+                Console.WriteLine("Invalid miles per gallon. Miles per gallon set to 0.");
+            }
+        }
+    }
+
+
+    // constructors
+    public Car()
+    {
+        make = "N/A";
+        model = "N/A";
+        year = DateTime.Now.Year;
+        maximumGasTankLevel = 100;
+        currentGasTankLevel = 100;
+        milesPerGallon = 100;
+    }
+    //car properties
+    public Car(string make, string model, int year, double maximumGasTankLevel, double currentGasTankLevel, double milesPerGallon)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.maximumGasTankLevel = maximumGasTankLevel;
+        this.currentGasTankLevel = currentGasTankLevel;
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public Car(Car car)
+    {
+        make = car.make;
+        model = car.model;
+        year = car.year;
+        maximumGasTankLevel = car.maximumGasTankLevel;
+        currentGasTankLevel = car.currentGasTankLevel;
+        milesPerGallon = car.milesPerGallon;
+    }
+
+
+    //determing how much gas was added and what the new amount would be
+    public string FillGasTank(double gallons)
+    {
+        if (gallons <= 0 || gallons > maximumGasTankLevel - currentGasTankLevel)
+        {
+            return "Invalid number of gallons.";
+        }
+        else
+        {
+            currentGasTankLevel += gallons;
+            return "Gas tank filled with " + gallons + " gallons.";
+        }
+    }
+
+    public bool CanDriveDistance(double distance)
+    {
+        return (distance / milesPerGallon <= currentGasTankLevel);
+    }
+
+    //calculating drive distance and if car has enough gas
+    public string Drive(double distance)
+    {
+        if (CanDriveDistance(distance))
+        {
+            currentGasTankLevel -= distance / milesPerGallon;
+            return "Car drove " + distance + " miles.";
+        }
+        else
+        {
+            return "Not enough gas to drive that distance.";
+        }
+    }
+
+    //calculating what should be returned based on  users input to current gas
+    public string Gas()
+    {
+        double gasPercentage = currentGasTankLevel / maximumGasTankLevel;
+        if (gasPercentage == 1)
+        {
+            return "Full tank";
+        }
+        else if (gasPercentage == 0.75)
+        {
+            return "3/4 tank";
+        }
+        else if (gasPercentage == 0.5)
+        {
+            return "1/2 tank";
+        }
+        else if (gasPercentage == 0.25)
+        {
+            return "1/4 tank";
+        }
+        else if (gasPercentage > 0)
+        {
+            return currentGasTankLevel + " out of " + maximumGasTankLevel + " gallons remaining";
+        }
+        else if (gasPercentage <= 0)
+        {
+            return "Empty tank";
+        }
+        else
+        {
+            return "";
+        }
+
+    }
+}
---
+++ Lab1/Driver.cs
@@ -0,0 +1,99 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // make a default Car and display it to the console
+
+
+
+        // asks the user for their car's information, creates the car, and displays it to the console
+
+        Console.WriteLine("------------Default Car--------------");
+        Console.WriteLine("Make: N/A");
+        Console.WriteLine("Model: N/A");
+        Console.WriteLine("Year:" + DateTime.Now.Year);
+        Console.WriteLine("Max Gas: 100");
+        Console.WriteLine("Current Gas: 100");
+        Console.WriteLine("Miles Per Gallon: 100");
+
+
+        Console.WriteLine("-------------Users Car-------------");
+        Console.Write("Enter your car's make: ");
+        string make = Console.ReadLine();
+        Console.Write("Enter your car's model: ");
+        string model = Console.ReadLine();
+        Console.Write("Enter your car's year: ");
+        int year = int.Parse(Console.ReadLine());
+        Console.Write("Enter your car's max gas tank level: ");
+        double maximumGasTankLevel = double.Parse(Console.ReadLine());
+        Console.Write("Enter your car's current gas tank level: ");
+        double currentGasTankLevel = double.Parse(Console.ReadLine());
+        Console.Write("Enter your car's miles per gallon: ");
+        double milesPerGallon = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("------------------------------------");
+        
+        Car userCar = new Car (make, model, year, maximumGasTankLevel, currentGasTankLevel, milesPerGallon);
+        Console.WriteLine("Make: " + make);
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Year: " + year);
+        Console.WriteLine("Maximum Gas Tank Level: " + maximumGasTankLevel);
+        Console.WriteLine("Current Gas Tank Level: " + currentGasTankLevel);
+        Console.WriteLine("Miles Per Gallon: " + milesPerGallon);
+
+
+        // display a menu to the user asking what they would like to do next
+        bool quit = false;
+        while (!quit)
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("What would you like to do next?\n1. Travel to Kingsport (25 Miles)\n2. Travel to Knoxville (108 Miles)\n3. Specify distance to travel\n4. Check on car details\n5. Fill gas tank\n6. Quit program");
+            Console.WriteLine("--------------------------");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(userCar.Drive(25));
+                    break;
+                case 2:
+                    Console.WriteLine(userCar.Drive(108));
+                    break;
+                case 3:
+                    Console.WriteLine("Enter distance to travel: ");
+                    double distance = double.Parse(Console.ReadLine());
+                    Console.WriteLine(userCar.Drive(distance));
+                    break;
+                case 4:
+                    Console.WriteLine("Make: " + make);
+                    Console.WriteLine("Model: " + model);
+                    Console.WriteLine("Year: " + year);
+                    Console.WriteLine("Maximum Gas Tank Level: " + maximumGasTankLevel);
+                    Console.WriteLine("Current Gas Tank Level: " + currentGasTankLevel);
+                    Console.WriteLine("Miles Per Gallon: " + milesPerGallon);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter gallons to fill: ");
+                    double gallons = double.Parse(Console.ReadLine());
+                    Console.WriteLine(userCar.FillGasTank(gallons));
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
